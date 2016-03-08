a589e37a1adc8bb28effaf091a8de110c72dd963
adamantike <mike@fmanganiello.com.ar>
2016-03-08 13:53:28 -0300

    Implementación de lógica para el movimiento de peones

--- models/Peon.py
+++ models/Peon.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 from .Pieza import Pieza
 
 
@@ -10,16 +12,27 @@ class Peon(Pieza):
 
     def puede_mover(self, tablero, x, y):
         resultado = False
-        if self.color == 'B':
-            # Movimiento frontal
-            if self.x == x and self.y - 1 == y:
-                resultado = not tablero.get_posicion(x, y)
-            # Movimiento para comer
-            elif x in [self.x - 1, self.x + 1] and self.y - 1 == y:
-                destino = tablero.get_posicion(x, y)
-                resultado = destino and destino.color != 'B'
-            # Movimiento frontal doble
-            elif self.x == x and self.y - 2 == y == 4:
-                resultado = not tablero.get_posicion(x, y)
+
+        # Determina la dirección del movimiento.
+        # Si el color del peón es blanco, su movimiento sobre el eje Y es
+        # negativo (avanza cuando se reduce el valor de y).
+        # Si el color del peón es negro, su movimiento sobre el eje Y es
+        # positivo.
+        direccion_y = -1 if self.color == 'B' else 1
+
+        # Lógica para los diferentes movimientos posibles del peón.
+        # Movimiento frontal
+        if self.x == x and self.y + direccion_y == y:
+            resultado = not tablero.get_posicion(x, y)
+        # Movimiento para comer
+        elif x in [self.x - 1, self.x + 1]and self.y + direccion_y == y:
+            destino = tablero.get_posicion(x, y)
+            resultado = destino and destino.color != 'B'
+        # Movimiento frontal doble
+        elif (self.x == x
+                and self.y + 2 * direccion_y == y
+                and ((self.color == 'N' and self.y == 1)
+                       or (self.color == 'B' and self.y == 6))):
+            resultado = not tablero.get_posicion(x, y)
 
         return resultado
